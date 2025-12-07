a6fb5b644203fdf8adea58a1ae0b81ac06cabca9
koiblue2800 <cmsoowordpress@gmail.com>
2025-12-07 17:33:19 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", () => {
+  if (!window.RufflePlayer) {
+    console.error("Ruffle no se cargó correctamente.");
+    return;
+  }
+
+  const ruffle = window.RufflePlayer.newest();
+  const container = document.getElementById("game-container");
+  const player = ruffle.createPlayer();
+
+  player.config = {
+    autoplay: "on",
+    unmuteOverlay: "hidden",
+    splashScreen: false
+  };
+
+  container.appendChild(player);
+  player.load("assets/ruffle/bomb-it-2.swf");
+});
